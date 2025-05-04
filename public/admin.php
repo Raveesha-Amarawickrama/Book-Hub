44e627b029489349e920e3b08ac2e34b023a3b0b
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-05-04 18:34:05 +0530

    Update admin.php
    add html  for admin dashboard

--- public/admin.php
+++ public/admin.php
@@ -19,3 +19,37 @@ $pending_requests_result = $conn->query($pending_requests_query);
 $total_pending_requests = $pending_requests_result->fetch_assoc()['total_pending_requests'];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/admin.css">
+    <script src="../js/script.js" defer></script>
+</head>
+
+<body>
+
+    <div class="admin-container">
+        <?php require_once 'admin_nav.php'; ?>
+
+        <!-- Main Content -->
+        <main class="admin-main">
+            <div class="stats">
+                <div class="dash_ico">
+                    <img src="../assets/bookicon.png" alt="" width="70%">
+                    <h2>Total Books: <?= $total_books; ?></h2>
+                </div>
+                <div class="dash_ico">
+                    <img src="../assets/bookreq.png" alt="" width="50%" style="margin-top:10%; margin-bottom:7%">
+                    <h2>Total Pending Book Requests: <?= $total_pending_requests; ?></h2>
+                </div>
+            </div>
+
+
+        </main>
+    </div>
+</body>
+
+</html>
