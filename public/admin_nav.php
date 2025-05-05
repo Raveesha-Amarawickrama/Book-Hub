2ab36f063352f1603e961ae770050cca61515e1c
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-05-06 00:30:36 +0530

    Update admin_nav.php

--- public/admin_nav.php
+++ public/admin_nav.php
@@ -13,3 +13,43 @@ if (!isset($_SESSION['user_id']) || $_SESSION['membership_type'] != 'admin') {
 // Get the current page for active tab highlighting
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin_nav.css">
+</head>
+
+<body>
+    <div class="admin-sidebar">
+
+        <ul>
+            <li>
+                <a href="admin.php" class="<?= $current_page == 'dashboard.php' ? 'active' : ''; ?>">
+                    Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="view_book_requests.php"
+                    class="<?= $current_page == 'view_book_requests.php' ? 'active' : ''; ?>">
+                    View Book Requests
+                </a>
+            </li>
+            <li>
+                <a href="add_book.php" class="<?= $current_page == 'add_book.php' ? 'active' : ''; ?>">
+                    Add New Book
+                </a>
+            </li>
+            <li>
+                <a href="manage_book.php" class="<?= $current_page == 'manage_book.php' ? 'active' : ''; ?>">
+                    Manage Books
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</body>
+
+</html>
