ac89cbabbca52776d959b719e67e65d5f164da5e
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-05-06 23:14:16 +0530

    Create profile.php

---
+++ public/profile.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once '../config/db.php';
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+    }
+
+$user_id = $_SESSION['user_id'];
